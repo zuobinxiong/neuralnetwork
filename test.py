8ad701de84be33f168d4feafe64a0826f6c2946c
Zuobin Xiong <zxiong2@gsu.edu>
2019-02-13 15:15:12 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import mnist_loader
+import network3
+import time
+from network3 import Network
+from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+
+training_data, validation_data, test_data = network3.load_data_shared()
+mini_batch_size = 10
+localtime = time.asctime(time.localtime(time.time()))
+print localtime
+net = Network([
+    FullyConnectedLayer(n_in=784, n_out=100),
+    SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+net.SGD(training_data, 60, mini_batch_size, 0.1,
+        validation_data, test_data)
+
